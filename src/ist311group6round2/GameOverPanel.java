57c5387bf84b497068ce5d6dc7542e9d19e048e9
Chelsea Martin <Chelsea Martin@104.39.93.129>
2015-02-23 20:56:44 -0500

    Merge origin/master
    Conflicts:
    	src/ist311group6round2/SplashPanel.java

--- src/ist311group6round2/GameOverPanel.java
+++ src/ist311group6round2/GameOverPanel.java
@@ -1,11 +1,27 @@
 package ist311group6round2;
 
+import java.awt.Image;
+import java.awt.Toolkit;
+import javax.swing.JButton;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+import javax.swing.JLabel;
+
 /**
  *
- * @author Stephen
+ * @author Chelsea
  */
 public class GameOverPanel extends MyJPanel
 {
-
+    JLabel loser = new JLabel("You lose");
+    JButton playagain= new JButton("Play again");
+   JButton exit = new JButton("Exit Game");
+   Image exitpacman = Toolkit.getDefaultToolkit().getImage("images/GameOver.png");
+    public GameOverPanel()
+    {
+        add(loser);
+        add(playagain);
+        add(exit);
+    }
+    
     
 }
